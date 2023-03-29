f0ed7b4d7c48def2112d3198abfcc23995fec17f
jinkwon9301 <valor9254@gmail.com>
2023-03-29 23:21:26 +0900

    스프링 MVC : 실용적인 방식

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -2,14 +2,11 @@ package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 @Controller
 public class SpringMemberSaveControllerV1 {
